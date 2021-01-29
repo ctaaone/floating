7f728e0e29f39b590a29e03a3198041bf10ee44d
Guest <guest-t1nsxs@h29859.sgb.skir.kr>
2021-01-29 19:35:44 +0900

    #3

--- algorithm/main.cpp
+++ algorithm/main.cpp
@@ -6,33 +6,31 @@ using namespace std;
 
 #define FOR(i,N) for(int (i)=0;(i)<(N);(i++))
 
-int arr[10][10],count;
+int arr[10][10],count,Num;
 
-int solve(int n,vector<int>& isPair){
-    
-    for(int i=n+1;i<10;i++){
-        if(arr[n][i]){
-            isPair[n]=1;
-            isPair[i]=1;
-            for(int j=0;j<10;j++){
-                if(isPair[j]==0){
-                    solve(j,isPair);
-                }
-            }
+void solve(int n,vector<int>& isPair){
+    int flag=0;
+    for(int i=n+1;i<Num;i++){
+        if(arr[n][i]&&isPair[i]==0){
+            isPair[n]=isPair[i]=1;
+            FOR(j,Num)if(!isPair[j])flag=1;if(!flag)count++;
+            for(int j=n+1;j<Num;j++)
+                if(!isPair[j]){solve(j,isPair);break;}
+            isPair[i]=0;
         }
     }
-
+    isPair[n]=0;
 }
 
 int main(){
 
-    int C, N, M;vector<int> isPair(10);
+    int C, M;vector<int> isPair(10);
     int a,b;
     scanf("%d",&C);
     while(C--){
         FOR(i,10)FOR(j,10)arr[i][j]=0;
         FOR(i,10)isPair[i]=0; count=0;
-        scanf("%d%d",&N,&M);
+        scanf("%d%d",&Num,&M);
         FOR(i,M){
             scanf("%d%d",&a,&b);
             arr[a][b]=1;arr[b][a]=1;
