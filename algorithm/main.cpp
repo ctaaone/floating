9b9a98e8c4eb8a5073c176bad1f9aa0310de8154
Guest <guest-grgvnl@h29859.sgb.skir.kr>
2021-02-15 23:02:45 +0900

    #6.5

--- algorithm/main.cpp
+++ algorithm/main.cpp
@@ -8,13 +8,16 @@ int C, N, M;
 string members, fans;
 vector<int> m,f;
 
-void normal(vector<int>&a){
-    while(a.size()&&a.back()==0)a.pop_back();
+void pv(vector<int>&a){
+    for(int i=a.size()-1;i>=0;i--)cout<<a[i];cout<<endl;
 }
+
 vector<int> addTo(vector<int>&a, vector<int>&b,int n){
-    int max_size = (a.size()+n)>b.size()?a.size()+n:b.size();
+    int max_size ;
+    if(a.size())max_size=(a.size()+n)>b.size()?a.size()+n:b.size();
+    else max_size=b.size();
     vector<int> c;
-    for(int i=0;i<max_size;i++)c.push_back((i<b.size()?b[i]:0)+((i-n>=0&&(i-n<a.size()))?a[i-n]:0));
+    for(int i=0;i<max_size;i++)c.push_back((i<b.size()?b[i]:0)+((i-n>=0&&i-n<a.size())?a[i-n]:0));
     return c;
 }
 vector<int> subTo(vector<int>&a, vector<int>&b){
@@ -25,7 +28,7 @@ vector<int> subTo(vector<int>&a, vector<int>&b){
 }
 
 vector<int> mul(vector<int>&a, vector<int>&b){
-    if(a.size()+b.size()==0)return vector<int>(0);
+    if(a.size()*b.size()==0)return vector<int>(0);
     vector<int> c(a.size()+b.size()-1,0);
     for(int i=0;i<b.size();i++){
         for(int j=0;j<a.size();j++){
@@ -35,12 +38,12 @@ vector<int> mul(vector<int>&a, vector<int>&b){
     return c;
 }
 vector<int> karatsuba(vector<int>& a, vector<int>&b){
-    for(int i=a.size()-1;i>=0;i--)cout<<a[i];cout<<endl;
-    for(int i=b.size()-1;i>=0;i--)cout<<b[i];cout<<endl;
+    //for(int i=a.size()-1;i>=0;i--)cout<<a[i];cout<<endl;
+    //for(int i=b.size()-1;i>=0;i--)cout<<b[i];cout<<endl<<endl;
     if(a.empty()||b.empty())return vector<int>(0);
-    if(a.size()<10)return mul(a,b);
+    if(a.size()<50)return mul(a,b);
     int half=a.size()/2;
-    cout<<"half"<<half<<endl;
+    //cout<<"half"<<half<<endl;
     vector<int> a0(a.begin(),a.begin()+half),a1(a.begin()+half,a.end());
     vector<int> b0(b.begin(),b.begin()+(half>b.size()?b.size():half)),b1(b.begin()+(half>b.size()?b.size():half),b.end());
     vector<int> z0=karatsuba(a1,b1);
@@ -49,9 +52,11 @@ vector<int> karatsuba(vector<int>& a, vector<int>&b){
     vector<int> res=karatsuba(a2,b2);
     vector<int> z2,temp;
     temp=addTo(z1,z0,0);
+    //cout<<"z1z0z2"<<endl;pv(z1);pv(z0);
     z2=subTo(temp,res);
-    temp=addTo(z0,z1,half);
-    res=addTo(temp,z2,half);
+    //pv(z2);cout<<endl;
+    temp=addTo(z0,z2,half);
+    res=addTo(temp,z1,half);
     return res;
 }
 
@@ -71,8 +76,8 @@ int main()
         }
         vector<int> result=karatsuba(f,m);
         int out=0;
-        cout<<"result ";for(int i=result.size()-1;i>=0;i--)cout<<result[i];cout<<endl;
-        for(int i=N-1;i<=M-1;i++){
+        //cout<<"result ";for(int i=result.size()-1;i>=0;i--)cout<<result[i];cout<<endl;
+        for(int i=N-1;i<M;i++){
             if(result[i]==0)out++;
         }
         cout<<out<<endl;
