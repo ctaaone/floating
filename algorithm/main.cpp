15cb30c02f4c1be34c202038aeda371231dcacb4
Guest <guest-t42ibv@h29859.sgb.skir.kr>
2021-02-16 19:58:39 +0900

    #7

--- algorithm/main.cpp
+++ algorithm/main.cpp
@@ -4,85 +4,66 @@
 
 using namespace std;
 
-int C, N, M;
-string members, fans;
-vector<int> m,f;
-
-void pv(vector<int>&a){
-    for(int i=a.size()-1;i>=0;i--)cout<<a[i];cout<<endl;
-}
-
-vector<int> addTo(vector<int>&a, vector<int>&b,int n){
-    int max_size ;
-    if(a.size())max_size=(a.size()+n)>b.size()?a.size()+n:b.size();
-    else max_size=b.size();
-    vector<int> c;
-    for(int i=0;i<max_size;i++)c.push_back((i<b.size()?b[i]:0)+((i-n>=0&&i-n<a.size())?a[i-n]:0));
-    return c;
-}
-vector<int> subTo(vector<int>&a, vector<int>&b){
-    int max_size = a.size()>b.size()?a.size():b.size();
-    vector<int> c;
-    for(int i=0;i<max_size;i++)c.push_back((i<a.size()?a[i]:0)-(i<b.size()?b[i]:0));
-    return c;
-}
-
-vector<int> mul(vector<int>&a, vector<int>&b){
+vector<int> multi(vector<int>&a, vector<int>&b){
     if(a.size()*b.size()==0)return vector<int>(0);
     vector<int> c(a.size()+b.size()-1,0);
-    for(int i=0;i<b.size();i++){
-        for(int j=0;j<a.size();j++){
-            c[i+j]+=a[j]*b[i];
+    for(int i=0;i<a.size();i++){
+        for(int j=0;j<b.size();j++){
+            c[i+j]+=a[i]*b[j];
         }
     }
     return c;
 }
-vector<int> karatsuba(vector<int>& a, vector<int>&b){
-    //for(int i=a.size()-1;i>=0;i--)cout<<a[i];cout<<endl;
-    //for(int i=b.size()-1;i>=0;i--)cout<<b[i];cout<<endl<<endl;
-    if(a.empty()||b.empty())return vector<int>(0);
-    if(a.size()<50)return mul(a,b);
-    int half=a.size()/2;
-    //cout<<"half"<<half<<endl;
-    vector<int> a0(a.begin(),a.begin()+half),a1(a.begin()+half,a.end());
-    vector<int> b0(b.begin(),b.begin()+(half>b.size()?b.size():half)),b1(b.begin()+(half>b.size()?b.size():half),b.end());
-    vector<int> z0=karatsuba(a1,b1);
-    vector<int> z1=karatsuba(a0,b0);
-    vector<int> a2=subTo(a1,a0),b2=subTo(b1,b0);
-    vector<int> res=karatsuba(a2,b2);
-    vector<int> z2,temp;
-    temp=addTo(z1,z0,0);
-    //cout<<"z1z0z2"<<endl;pv(z1);pv(z0);
-    z2=subTo(temp,res);
-    //pv(z2);cout<<endl;
-    temp=addTo(z0,z2,half);
-    res=addTo(temp,z1,half);
-    return res;
+void addTo(vector<int>&a, vector<int>&b,int n){
+    if(b.size()==0)return;
+    else if(a.size()==0){
+        for(int i=0;i<n;i++)a.push_back(0);
+        for(int i=0;i<b.size();i++)a.push_back(b[i]);
+        return ;
+    }
+    for(int i=a.size();i<b.size()+n;i++)a.push_back(0);
+    for(int i=n;i<b.size()+n;i++)a[i]+=b[i-n];
+}
+void subTo(vector<int>&a, vector<int>&b){
+    if(b.size()==0)return;
+    else if(a.size()==0){
+        for(int i=0;i<b.size();i++)a.push_back(-b[i]);return;
+    }
+    for(int i=a.size();i<b.size();i++)a.push_back(0);
+    for(int i=0;i<b.size();i++)a[i]-=b[i];
 }
 
+vector<int> karatsuba(vector<int>&a, vector<int>&b){
+    if(b.size()*a.size()==0)return vector<int>(0);
+    if(a.size()<500)return multi(a,b);
+    int half=a.size()/2;
+    vector<int> a0(a.begin(),a.begin()+half), a1(a.begin()+half,a.end());
+    vector<int> b0(b.begin(),b.begin()+(half<b.size()?half:b.size())), b1(b.begin()+(half<b.size()?half:b.size()),b.end());
+    vector<int> z1=karatsuba(a1,b1),z0=karatsuba(a0,b0);
+    subTo(a1,a0);subTo(b1,b0);
+    vector<int> temp=karatsuba(a1,b1);
+    vector<int> z2;addTo(z2,z1,0);addTo(z2,z0,0);subTo(z2,temp);
+    vector<int> res;
+    addTo(res,z0,0);addTo(res,z2,half);addTo(res,z1,half*2);
+    return res;
+}
+string members,fans;
 
-
-int main()
-{
+int main(){
+    int C;
     cin>>C;
     while(C--){
-        cin>>members;
-        cin>>fans;
-        N=members.size();
-        M=fans.size();
-        for(int i=0;i<M;i++){
-            f.push_back(fans[i]=='M'?1:0);
+        vector<int> m,f;
+        cin>>members;int N=members.size();
+        cin>>fans;int M=fans.size();
+        for(int i=0;i<fans.size();i++){
             if(i<N)m.push_back(members[N-1-i]=='M'?1:0);
+            f.push_back(fans[i]=='M'?1:0);
         }
-        vector<int> result=karatsuba(f,m);
+        vector<int> res=karatsuba(f,m);
         int out=0;
-        //cout<<"result ";for(int i=result.size()-1;i>=0;i--)cout<<result[i];cout<<endl;
-        for(int i=N-1;i<M;i++){
-            if(result[i]==0)out++;
-        }
+        for(int i=N-1;i<M;i++)if(res[i]==0)out++;
         cout<<out<<endl;
-        while(m.size())m.pop_back();
-        while(f.size())f.pop_back();
     }
     return 0;
 }
