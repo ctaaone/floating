4844203059c179a8592124753931d0c0769f1599
Guest <guest-g4vfd4@h29865.sgb.skir.kr>
2021-02-20 17:10:47 +0900

    #7.5

--- algorithm/main.cpp
+++ algorithm/main.cpp
@@ -1,55 +1,31 @@
 #include <iostream>
 #include <memory.h>
-#include <string>
-#include <algorithm>
-#include <vector>
 
 using namespace std;
 
 
-int N;
-string str,name;
-int cache[100][100];
-vector<string> result;
+int N,arr[501]={-1},cache[501];
 
-int solve(int si, int di){//source index, destination index
-    if(si==str.size()&&di==name.size())return 1;
-    else if(!(si<str.size()&&di<name.size())) return 0;
-    int &ret=cache[si][di];
+int max(int &a, int &b){
+    return a>b?a:b;
+}
+
+int solve(int index){
+    int &ret=cache[index];
     if(ret!=-1)return ret;
-    while(si<str.size()&&di<name.size()){
-        if(str[si]!='*'&&str[si]!='?'){
-            if(str[si]!=name[di])return 0;
-            si++;di++;
-        }else if(str[si]=='?'){
-            si++;di++;
-        }else{
-            while(si+1<str.size()&&str[si+1]=='*')si++;
-            for(int i=di;i<=name.size();i++){
-                if(solve(si+1,i))return ret=1;
-            }return ret=0;
-        }
-    }
-    while(si<str.size()&&str[si]=='*')si++;
-    if(si==str.size()&&di==name.size())return ret=1;
-    return ret=0;
+    int ma=1;
+    for(int i=index+1;i<=N;i++)
+        if(arr[index]<arr[i])ma=max(ma, solve(i)+1);
+    return ret=ma;
 }
-int comp(string a, string b){
-    return a<b;
-}
+
 int main(){
     int C;cin>>C;
     while(C--){
-        cin>>str;
-        cin>>N;
-        for(int i=0;i<N;i++){
-            memset(cache, -1, sizeof(cache));
-            cin>>name;
-            if(solve(0,0))result.push_back(name);
-        }
-        sort(result.begin(),result.end(),comp);
-        for(int i=0;i<result.size();i++)cout<<result[i]<<"\n";
-        while(!result.empty())result.pop_back();
+        cin>>N;int ma=0;
+        for(int i=1;i<=N;i++)cin>>arr[i];
+        memset(cache, -1, sizeof(cache));
+        cout<<solve(0)-1<<"\n";
     }
     return 0;
 }
