7cf7326c6ff759fc337eaa4a74c53f21521d08ba
Guest <guest-qynoa3@h29859.sgb.skir.kr>
2021-03-18 19:45:34 +0900

    메롱

--- algorithm/main.cpp
+++ algorithm/main.cpp
@@ -1,18 +1,34 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <memory.h>
 
 using namespace std;
 
-int N,W;vector<string> list;
+int N,W,cache[1001][100];
+vector<string> list;vector<int> weight;vector<int> des;vector<int> path;
+int solve(int w, int in){
+    if(in==N)return 0;
+    int &ret=cache[w][in];if(ret!=-1)return ret;
+    if(w-weight[in]<0){return ret=solve(w,in+1);}
+    int a,b;
+    a=solve(w-weight[in],in+1)+des[in];
+    b=solve(w,in+1);
+    if(a>b){path.push_back(in);return ret=a;}
+    return ret=b;
+}
 
 int main(){
     int C;cin>>C;
     while(C--){
-        cin>>N>>W;
+        cin>>N>>W;memset(cache,-1,sizeof(cache));
         for(int i=0;i<N;i++){
-            string temp;
-            cin>>temp;list.
+            string t1;int t2,t3;
+            cin>>t1>>t2>>t3;
+            list.push_back(t1);weight.push_back(t2);des.push_back(t3);
         }
+        printf("%d",solve(W,0));printf(" %d\n",(int)path.size());
+        while(!path.empty()){cout<<list[path.back()]<<endl;path.pop_back();}
+        for(int i=0;i<N;i++){list.pop_back();weight.pop_back();des.pop_back();}
     }
 }
