e2574f198e0f1ee87602534954935c9e6a361fb3
Guest <guest-twekqr@h29859.sgb.skir.kr>
2021-04-03 20:20:14 +0900

    #16

--- algorithm/main.cpp
+++ algorithm/main.cpp
@@ -5,47 +5,73 @@
 
 using namespace std;
 
-int K,cache[1<<16][15],choice[15][15],exS[15];vector<string> str;
+int K,cache[1<<16][15],choice[15][15],choiceLen[15][15],maxIn;vector<string> str;
 
 
-void order(){
+int order(){
     string temp;
+    int used=0;
     for(int i=0;i<K;i++){
-        if(exS[i]==1)continue;
+        if(used&(1<<i))continue;
         for(int j=i+1;j<K;j++){
-            if(exS[j]==1) continue;
+            if(used&(1<<i)) break;
+            if(used&(1<<j)) continue;
+            
             string a,b;
             if(str[i].size()>str[j].size()){a=str[i];b=str[j];}
             else {a=str[j];b=str[i];}
+
             for(int k=0;k<=a.size()-b.size();k++){
                 int flag=1;
                 for(int l=0;l<b.size();l++){
-                    if(a[k+l]!=b[l]){flag=0;break;}
+                    if(a[k+l]!=b[l]){
+                        flag=0;break;
+                    }
                 }
                 if(flag){
-                    
+                    if(str[i].size() < str[j].size()){
+                        used |= (1<<i); break;
+                    }
+                    used |= (1<<j); break;
                 }
             }
         }
     }
+    return used;
 }
 int appN(int a, int b){
-
+    string A = str[a], B = str[b];
+    int isWrong, ret = B.size();
+    for(int k=1;k<=A.size()||k<=B.size();k++){
+        for(int i=0;i<k;i++)
+        if(A[A.size() - k + i] != B[i]){
+            isWrong = 1; break;
+        }
+        if(isWrong)break;
+        ret--;
+    }
+    return ret;
 }
-int min(int &a, int &b){
-    return a<b?a:b;
+
+void getPath(int used){
+    int cur = 0;
+    for(int i=0;i<k;i++){
+        
+    }
 }
 
 int minLen(int used, int cur, int in){
+    if(in>maxIn)maxIn = in;
     int &ret = cache[used][cur]; if(ret != -1)return ret;
     if(used == (1<<K) - 1)return ret = 0;
     ret = 0;int temp;
     for(int i=0;i<K;i++){
         if( ((1<<i)&used) == 1)continue;
-        temp = appN(cur,i)+minLen(used|(1<<i),i,in+1);
+        temp = appN(cur,i) + minLen(used|(1<<i),i,in+1);
         if(ret > temp){
             ret = temp;
             choice[in][cur] = i;
+            choiceLen[in][cur] = temp - minLen(used|(1<<i), i, in+1);
         }
     }
     return ret;
@@ -55,10 +81,12 @@ int main(){
     int C;cin>>C;
     while(C--){
         cin>>K;string temp;memset(cache, -1, sizeof(cache));memset(choice,-1,sizeof(choice));
-        memset(exS, 0, sizeof(exS));
-        for(int i=0;i<K;i++){cin>>temp;str.push_back(temp);}order();
+        memset(choiceLen, 0, sizeof(choiceLen)); maxIn = 0;
+        for(int i=0;i<K;i++){cin>>temp;str.push_back(temp);}
+        int used = order();
 
-        for(int i=0;i<K;i++)minLen(1<<i,i,0);
+        for(int i=0;i<K;i++)minLen(used|(1<<i),i,0);
+        getPath(used);
 
         while(!str.empty())str.pop_back();
     }return 0;
