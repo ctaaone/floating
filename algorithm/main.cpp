44420b498ba5f690ccbd589de569cdfbae9fd79f
Guest <guest-qfs248@h29859.sgb.skir.kr>
2021-04-12 17:22:46 +0900

    #21

--- algorithm/main.cpp
+++ algorithm/main.cpp
@@ -1,130 +1,58 @@
 #include <iostream>
+#include <vector>
 #include <memory.h>
 
 using namespace std;
 
-char board[5][5];char cache[1<<25];
-int TRI[4][3][2] = {{{0,0},{1,0},{1,1}}, //ㄱ
-                    {{1,0},{1,1},{0,1}},
-                    {{0,0},{1,1},{0,1}}, //ㄴ
-                    {{0,0},{1,0},{0,1}}};
-int BI[2][2][2] = {{{0,0},{1,0}}, //ㅡ
-                   {{0,0},{0,1}}}; //ㅣ
+inline int cell(int y, int x) {return 1<<(5*y+x);}
 
-int bton(char b[][5]){
-    int ret=0;
-    for(int i=0;i<5;i++){
+char cache[1<<25];
+vector<int> blocks;
+
+void precal(){
+    for(int y=0;y<4;y++){
+        for(int x=0;x<4;x++){
+            vector<int> cells;
+            for(int dy=0;dy<2;dy++)for(int dx=0;dx<2;dx++)cells.push_back(cell(y+dy, x+dx));
+            int sqare = cells[0] + cells[1] + cells[2] + cells[3];
+            for(int i=0;i<4;i++)blocks.push_back(sqare - cells[i]);
+        }
+    }
+    for(int i=0;i<4;i++){
         for(int j=0;j<5;j++){
-            ret*=2;
-            ret+=(b[i][j]=='.'?0:1);
+            blocks.push_back(cell(i,j)+cell(i+1,j));
+            blocks.push_back(cell(j,i)+cell(j,i+1));
+        }
+    }
+}
+
+int bton(char cur[][5]){
+    int ret=0;
+    for(int y=0;y<5;y++){
+        for(int x=0;x<5;x++){
+            ret=(ret<<1);
+            if(cur[y][x]=='#')ret++;
         }
     }
     return ret;
 }
 
-//1 to possible 0 to impo'
-int check(char cur[][5], int x, int y, int type){ //0~3 - triblock 4~5 - biblock
-    if(type<=3){
-        for(int i=0;i<3;i++){
-            int X = x + TRI[type][i][0];
-            int Y = y + TRI[type][i][1];
-            if(X > 4 || Y > 4) return 0;
-            if(cur[X][Y] == '#') return 0;
-        }
-    }else{
-        type -= 4;
-        for(int i=0;i<2;i++){
-            int X = x + BI[type][i][0];
-            int Y = y + BI[type][i][1];
-            if(X > 4 || Y > 4) return 0;
-            if(cur[X][Y] == '#') return 0;
-        }
+char solve(int cur){
+    char & ret = cache[cur]; if(ret != -1) return ret;
+    ret = 0;
+    for(int i=0;i<blocks.size();i++){
+        if((cur&blocks[i])==0)
+            if(!solve(cur|blocks[i])) return ret = 1;
     }
-    return 1;
-}
-void modify(char cur[][5], int x, int y, int type, int put){
-    if(type<=3){
-        for(int i=0;i<3;i++){
-            int X = x + TRI[type][i][0];
-            int Y = y + TRI[type][i][1];
-            cur[X][Y] = (put==0?'.':'#');
-        }
-    }else{
-        type -= 4;
-        for(int i=0;i<2;i++){
-            int X = x + BI[type][i][0];
-            int Y = y + BI[type][i][1];
-            cur[X][Y] = (put==0?'.':'#');
-        }
-    }
-}
-
-char max(char &a, char &b){
-    return a>b?a:b;
-}
-
-int opti(char cur[][5]){
-    char temp[5][5]; int ret;
-    //#2
-    for(int i=0;i<5;i++){
-        for(int j=0;j<5;j++){
-            temp[4-j][i] = cur[i][j];
-        }
-    }
-    ret = bton(temp);
-    if(cache[ret]!=0)return ret;
-    //#3
-    for(int i=0;i<5;i++){
-        for(int j=0;j<5;j++){
-            temp[4-i][4-j] = cur[i][j];
-        }
-    }
-    ret = bton(temp);
-    if(cache[ret]!=0)return ret;
-    //#4
-    for(int i=0;i<5;i++){
-        for(int j=0;j<5;j++){
-            temp[j][4-i] = cur[i][j];
-        }
-    }
-    ret = bton(temp);
-    if(cache[ret]!=0)return ret;
-    return bton(cur);
-}
-
-char solve(char cur[][5]){//1 win -1 lose
-    char &ret = cache[opti(cur)]; if(ret != 0)return ret;
-
-    //BC
-    ret = -1;
-
-    /*printf("\n$$\n");
-    for(int i=0;i<5;i++){
-        for(int j=0;j<5;j++){
-            printf("%c",cur[i][j]);
-        }
-        printf("\n");
-    }printf("\n$$\n");*/
-
-    for(int y=0; y<5; y++){
-        for(int x=0; x<5; x++){
-            for(int type=0; type<6; type++){
-                if(check(cur, x, y, type)){
-                    modify(cur, x, y, type, 1);
-                    ret = max(ret, (char)-solve(cur));
-                    modify(cur, x, y, type, 0);
-                }
-            }
-        }
-    }
-
     return ret;
 }
 
 int main(){
-    int C;cin>>C;memset(cache, 0, sizeof(cache));
+    int C;cin>>C;memset(cache, -1, sizeof(cache));precal();
+    char board[5][5];
     while(C--){
         for(int i=0;i<5;i++)scanf("%s",board[i]);
-        cout<<((solve(board)==1)?"WINNING\n":"LOSING\n");
-    }return 0;
+        cout<<(solve(bton(board))?"WINNING\n":"LOSING\n");
+    }
+    return 0;
 }
