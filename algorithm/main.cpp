1f6a96d132414d87da3ccbd9125988c5cdda0358
이승재 <ctaaone5386@gmail.com>
2021-04-14 09:42:47 +0000

    1

--- algorithm/main.cpp
+++ algorithm/main.cpp
@@ -3,31 +3,16 @@
 
 using namespace std;
 
-int N,M,A[101],B[101],cache[101][101];
 
-int max(int&a,int&b){
-    return a>b?a:b;aaaaa
-}
-
-int solve(int a,int b){
-    int ret=cache[a][b],ma=0;
-    if(ret!=-1)return ret;
-    for(int i=a+1;i<=N;i++){
-        for(int j=b+1;j<=M;j++){
-            if()ma=max(ma,solve(i,j));
-        }
-    }
-
+int func1(){
+	return 1;
 }
 
 int main(){
-    int C;cin>>C;
-    while(C--){
-        cin>>N>>M;
-        for(int i=1;i<=N;i++)cin>>A[i];
-        for(int i=1;i<=M;i++)cin>>B[i];
-        memset(cache,-1,sizeof(cache));
-        cout<<solve(0,0)-1<<endl;
-    }
-    return 0;
+	int (*F)() = func1;
+	printf("%p\n",F);
+	for(int i=0;i<12;i++)printf("%02X",((unsigned char *)F)[i]);
+	//((unsigned char*)F)[5] = (unsigned char)2;
+	printf("\n%d", func1());
+	return 0;
 }
