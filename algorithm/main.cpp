c35761b64e2111be1c899754c2bb929edf80455c
이승재 <ctaaone5386@gmail.com>
2021-04-14 10:33:36 +0000

    #22

--- algorithm/main.cpp
+++ algorithm/main.cpp
@@ -1,73 +1,36 @@
 #include <iostream>
-#include <vector>
 #include <memory.h>
+
+inline int max(int a, int b) {return a>b?a:b;}
 
 using namespace std;
 
-<<<<<<< HEAD
 
-int func1(){
-	return 1;
+int N,M;
+int S[20][2]; //0 price 1 preference
+int cache[20][21474836];
+
+
+int solve(int index, int budjet){
+	int &ret = cache[index][budjet]; if(ret != -1)return ret;
+	int temp = budjet; ret = 0;
+	//BC
+	if(index == N-1)return ret = (budjet/S[index][0])*S[index][1];
+	
+	for(int i=0;budjet-i*S[index][0]>0;i++){
+		ret = max(solve(index+1,budjet-i*S[index][0])+i*S[index][1],ret);
+	}
+	
+	return ret;
 }
 
+
 int main(){
-	int (*F)() = func1;
-	printf("%p\n",F);
-	for(int i=0;i<12;i++)printf("%02X",((unsigned char *)F)[i]);
-	//((unsigned char*)F)[5] = (unsigned char)2;
-	printf("\n%d", func1());
+	int C;cin>>C;
+	while(C--){
+		cin>>N>>M;for(int i=0;i<20;i++)for(int j=0;j<21474836;j++)cache[i][j] = -1;
+		for(int i=0;i<N;i++)cin>>S[i][0]>>S[i][1];
+		cout<<solve(0,M/100)<<endl;
+	}
 	return 0;
-=======
-inline int cell(int y, int x) {return 1<<(5*y+x);}
-
-char cache[1<<25];
-vector<int> blocks;
-
-void precal(){
-    for(int y=0;y<4;y++){
-        for(int x=0;x<4;x++){
-            vector<int> cells;
-            for(int dy=0;dy<2;dy++)for(int dx=0;dx<2;dx++)cells.push_back(cell(y+dy, x+dx));
-            int sqare = cells[0] + cells[1] + cells[2] + cells[3];
-            for(int i=0;i<4;i++)blocks.push_back(sqare - cells[i]);
-        }
-    }
-    for(int i=0;i<4;i++){
-        for(int j=0;j<5;j++){
-            blocks.push_back(cell(i,j)+cell(i+1,j));
-            blocks.push_back(cell(j,i)+cell(j,i+1));
-        }
-    }
-}
-
-int bton(char cur[][5]){
-    int ret=0;
-    for(int y=0;y<5;y++){
-        for(int x=0;x<5;x++){
-            ret=(ret<<1);
-            if(cur[y][x]=='#')ret++;
-        }
-    }
-    return ret;
-}
-
-char solve(int cur){
-    char & ret = cache[cur]; if(ret != -1) return ret;
-    ret = 0;
-    for(int i=0;i<blocks.size();i++){
-        if((cur&blocks[i])==0)
-            if(!solve(cur|blocks[i])) return ret = 1;
-    }
-    return ret;
-}
-
-int main(){
-    int C;cin>>C;memset(cache, -1, sizeof(cache));precal();
-    char board[5][5];
-    while(C--){
-        for(int i=0;i<5;i++)scanf("%s",board[i]);
-        cout<<(solve(bton(board))?"WINNING\n":"LOSING\n");
-    }
-    return 0;
->>>>>>> 44420b498ba5f690ccbd589de569cdfbae9fd79f
 }
