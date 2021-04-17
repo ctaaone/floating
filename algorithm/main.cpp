e060911b40fe0632bc30cbaffadfa9174545f8c5
이승재 <ctaaone5386@gmail.com>
2021-04-17 10:34:13 +0000

    #24

--- algorithm/main.cpp
+++ algorithm/main.cpp
@@ -1,35 +1,25 @@
 #include <iostream>
 #include <memory.h>
+#include <cmath>
+#include <vector>
 
-inline int max(int a, int b) {return (a)>(b)?(a):(b);}
 
 using namespace std;
 
-int N,M, temp;
-int S[20][2];
 
-int solve(){
-	int C[201];memset(C, -1, sizeof(C));
-	for(int i=0;i<=M;i++){
-		int cur = i%201;
-		for(int j=0;j<N;j++)
-			if(i+S[j][0]>M)continue;
-			else C[(cur+S[j][0])%201] = max(C[(cur+S[j][0])%201],C[cur]+S[j][1]);
-	}
-	int ret=0;
-	for(int i=0;i<201;i++)ret = max(ret, C[i]);
-	return ret;
-}
+int N, K, M;
+int len[50],list[50];
+
 
 int main(){
-	int C;cin>>C;
+	int C; cin>>C;
 	while(C--){
-		cin>>N>>M; M/=100;
-		for(int i=0;i<N;i++){
-			cin>>temp>>S[i][1];
-			S[i][0] = temp/100;
-		}
-		cout<<solve()<<endl;
+		vector<vector<double>> P(N,vector<double>(N));
+		cin>>N>>K>>M;
+		for(int i=0;i<N;i++)scanf("%d",len+i);
+		for(int i=0;i<N;i++)for(int j=0;j<N;j++)scanf("%lf",&P[j][i]);
+		for(int i=0;i<N;i++)scanf("%d",list+i);
+		
 	}
-	return 1;
+	return 0;
 }
