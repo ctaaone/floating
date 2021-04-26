155fa028f2bace19ab6abadf22fd7e2b75010c1f
Guest <guest-qifbvg@h29859.sgb.skir.kr>
2021-04-26 19:35:42 +0900

    .

--- algorithm/main.cpp
+++ algorithm/main.cpp
@@ -12,6 +12,34 @@ using namespace std;
 int N, K, M;
 int len[50],list[10];
 
+vector<vector<double>> multi(vector<vector<double>> A, vector<vector<double>> B){
+	int R = A.size(), C = B[0].size(), L = B.size();
+	vector<vector<double>> res(R, vector<double>(C));
+	for(int r=0;r<R;r++){
+		for(int c=0;c<C;c++){
+			for(int l=0;l<L;l++){
+				res[r][c] += A[r][l] * B[l][c];
+			}
+		}
+	}
+	return res;
+}
+
+vector<vector<double>> pow(int n, vector<vector<double>> M){
+	if(n == 1) return M;
+	if(n%2==0)
+		return multi(pow(n/2, M), pow(n/2, M));
+	return multi(pow(n-1, M), M);
+}
+void printV(vector<vector<double>> &A){
+	printf("\nPV\n");
+	for(int i=0;i<A.size();i++){
+		for(int j=0;j<A[0].size();j++){
+			printf("%.3lf ",A[i][j]);
+		}printf("\n");
+	}
+}
+
 void solve(vector<vector<double>>& RM, vector<vector<double>>& P){
 	int k = 0;
 	int next, cur;
@@ -33,18 +61,25 @@ void solve(vector<vector<double>>& RM, vector<vector<double>>& P){
 		}
 		return ;
 	}
-	vector<vector<double>> M(4*N, vector<double>(4*N));
+	vector<vector<double>> M(4*N, vector<double>(4*N)),R;
 	for(int i=0;i<3*N;i++)
 		M[i][i+N] = 1.0;
 	for(int r=3*N;r<4*N;r++){
 		for(int k1=0;k1<4;k1++){
 			for(int c=0;c<N;c++){
 				next = r; cur = c;
-				if(len[cur]+k1==4) M[r][k1*c] = P[k1][cur] * RM[cur][next];
+				if(len[cur]+k1==4) M[r][(k1+1)*c] = RM[cur][next];
 			}
 		}
 	}	//M initialize
-	
+	printV(P);printV(M);
+	R = pow(K-3, M);
+	P = multi(R, P);
+	printV(R);printV(P);
+
+	for(int i=0;i<N;i++){
+		for(int j=2;j>=0&&len[i]>3-j;j--)P[3][i] += P[j][i];
+	}
 }
 
 
@@ -58,10 +93,11 @@ int main(){
 		for(int i=0;i<N;i++)for(int j=0;j<N;j++)scanf("%lf", &RM[i][j]);
 		for(int i=0;i<M;i++)scanf("%d",list+i);
 
+		P[0][0] = 1.0;
 		solve(RM, P);
 
 
-		for(int i=0;i<M;i++)printf("%.8lf ", P[K][list[i]]);
+		for(int i=0;i<M;i++)printf("%.8lf ", P[(K<4?K:3)][list[i]]);
 		printf("\n");
 
 	}
