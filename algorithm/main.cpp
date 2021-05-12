22c7913d376761bafa340abdb63e30bbb90d818a
Guest <guest-fa11p7@h29865.sgb.skir.kr>
2021-05-12 19:28:45 +0900

    #32

--- algorithm/main.cpp
+++ algorithm/main.cpp
@@ -3,15 +3,29 @@
 
 using namespace std;
 
+int N, INF = (int)2e9;
 
-int N;
+int max(int a, int b){
+	return a>b?a:b;
+}
+
+int solve(vector<int> arr, int total){
+	if(total==1) return 0; //BC
+	int ret = INF;
+	for(int i=0;i<total;i++){
+		for(int j=0;j<total;j++){
+			if(i==j)continue;
+
+		}
+	}
+}
 
 int main(){
 	int T;cin>>T;
 	while(T--){
-		vector<pair<int,int>> time;
-		cin>>N;
-		for(int i=0;i<N;i++)cin>>time.first
+		cin>>N;vector<int> arr(100);
+		for(int i=0;i<N;i++)cin>>arr[i];
+		cout<<solve(arr,N)<<endl;
 	}
 	return 0;
 }
