b28a21f236db4f9005499b5bd7f7c8260ae75579
Guest <guest-tmhfzh@h29859.sgb.skir.kr>
2021-05-13 19:36:52 +0900

    #33

--- algorithm/main.cpp
+++ algorithm/main.cpp
@@ -1,31 +1,32 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
-int N, INF = (int)2e9;
+int N;
 
 int max(int a, int b){
 	return a>b?a:b;
 }
 
-int solve(vector<int> arr, int total){
-	if(total==1) return 0; //BC
-	int ret = INF;
-	for(int i=0;i<total;i++){
-		for(int j=0;j<total;j++){
-			if(i==j)continue;
-
-		}
+int solve(vector<int> arr){
+	int ret = 0;
+	for(int i=1;i<N;i++){
+		sort(arr.begin(), arr.end(), greater<int>());
+		arr[arr.size()-2] += arr.back();
+		arr.pop_back();
+		ret+=arr.back();		
 	}
+	return ret;
 }
 
 int main(){
 	int T;cin>>T;
 	while(T--){
-		cin>>N;vector<int> arr(100);
+		cin>>N;vector<int> arr(N);
 		for(int i=0;i<N;i++)cin>>arr[i];
-		cout<<solve(arr,N)<<endl;
+		cout<<solve(arr)<<endl;
 	}
 	return 0;
 }
