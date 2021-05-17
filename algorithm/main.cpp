2de7d4314e52345627846b2cc43951bc30625920
Guest <guest-ijpnwj@h29859.sgb.skir.kr>
2021-05-17 19:32:44 +0900

    #34

--- algorithm/main.cpp
+++ algorithm/main.cpp
@@ -8,6 +8,24 @@ using namespace std;
 
 int N;
 
+int isTarget(int cStart, int cEnd, int tStart, int tEnd){
+	if(cStart<cEnd){
+		if(cStart<tStart && tStart<=cEnd &&!(tStart<tEnd&&tEnd<=cEnd)){
+			return 1;
+		}
+	}
+	else{
+		if(tStart<=cEnd || tStart>cStart){
+			if(tStart<tEnd){
+				if(!(tStart<tEnd&&tEnd<=cEnd))return 1;
+			}else if(cEnd<tEnd){
+				return 1;
+			}
+		}
+	}
+	return 0;
+}
+
 int solve(vector<pair<double,pair<double,double>>> arr){
 	sort(arr.begin(), arr.end(),greater<pair<double,pair<double,double>>>());
 	vector<pair<double,double>> rIndex(N);
@@ -24,9 +42,28 @@ int solve(vector<pair<double,pair<double,double>>> arr){
 		rIndex[i].second = theta1 + theta2 - (theta1+theta2>2*M_PI?2*M_PI:0);//A ending point
 	}
 	
-	double iStart, cStart, cEnd, tStart, tEnd, max;int count = 1;
+	double tR, cStart, cEnd, tStart, tEnd, max;int count;
 	for(int i=0;i<N;i++){  //Total iteration
-		
+		tR = 0; count = 1; cStart = rIndex[i].first; cEnd = rIndex[i].second;
+		while(1){
+			int findFlag = 0;
+			tR += cEnd - cStart + (cStart>cEnd?2*M_PI:0);
+			if(tR >= 2*M_PI) return count;
+			max = cEnd;
+			for(int j=0;j<N;j++){ //Searching for the maximum next circle
+				tStart = rIndex[j].first; tEnd = rIndex[j].second;
+				if(isTarget(cStart,cEnd,tStart,tEnd)){
+					if(max == cEnd) {max = tEnd; findFlag = 1; cStart = tStart; cEnd = tEnd; }
+					else if(cEnd<max){
+						if(tEnd > max || tEnd < cEnd) {max = tEnd; findFlag = 1; cStart = tStart; cEnd = tEnd; }
+					}else{
+						if(tEnd > max && tEnd < cEnd) {max = tEnd; findFlag = 1; cStart = tStart; cEnd = tEnd;}
+					}
+				}
+			}
+			if(!findFlag) break;
+			count++;
+		}		
 	}
 	return -1; //If fail
 }
