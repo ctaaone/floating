44a4573372fff34fbd79aa9bd6319af955ef6975
Guest <guest-avr08k@h29859.sgb.skir.kr>
2021-06-01 20:20:59 +0900

    #37

--- algorithm/main.cpp
+++ algorithm/main.cpp
@@ -1,67 +1,78 @@
-#include <vector>
 #include <iostream>
-#include <utility>
-#include <cmath>
-#include <algorithm>
 
 using namespace std;
 
-int N;
 
-int min(int a, int b){return a<b?a:b;}
+int H,W,R,C;
+char board[10][10], piece[10][10], P[4][10][10];
 
-int lsolve(vector<pair<double, double>>& rad, double S, double E){
-	double cS = S, mE = S;
-	int index = 0, count=1, cFlag;
-	
-	while(cS<E){
-		cFlag = 0;
-		for(;index<N&&rad[index].first<=cS;index++){
-			double start = rad[index].first, end = rad[index].second;
-			if(mE<end || start>end){
-				mE=end;cFlag=1;
-				if(start>end)break;
-			}
-		}
-		if(!cFlag)return 2e9;
-		cS = mE; count++;
+void piece_opti(){
+	int xl,xr,yt,yb;
+	for(int i=0;i<C;i++){
+		xl=i;
+		for(int j=0;j<R;j++)if(piece[j][i]=='#')break;
 	}
-	return count;
+	for(int i=C-1;i>=0;i--){
+		xr=i;
+		for(int j=0;j<R;j++)if(piece[j][i]=='#')break;
+	}
+	for(int i=0;i<R;i++){
+		yt=i;
+		for(int j=0;j<C;j++)if(piece[i][j]=='#')break;
+	}
+	for(int i=R-1;i>=0;i--){
+		yb=i;
+		for(int j=0;j<C;j++)if(piece[i][j]=='#')break;
+	}
+	for(int i=0;i<=yb-yt;i++){
+		for(int j=0;j<=xr-xl;j++){
+			piece[i][j] = piece[i+yt][j+xl];
+		}
+	}
+	R=yb-yt+1;C=xr-xl+1;
+	for(int y=0;y<R;y++){
+		for(int x=0;x<C;x++){
+			P[0][y][x] = piece[y][x];
+			P[2][x][R-y] = piece[y][x];
+			P[1][R-y][C-x] = piece[y][x];
+			P[3][C-x][y] = piece[y][x];
+		}
+	}
 }
 
-int csolve(vector<pair<double, pair<double, double>>>& par){
-	vector<pair<double, double>> rad(N);
-	vector<pair<double, double>> nrad(N);
-	double theta1, theta2;
-	for(int i=0;i<N;i++){
-		double x = par[i].second.first, y = par[i].second.second, r = par[i].first;
-		theta1 = atan2(y, x);
-		theta2 = 2*asin(r/16);
-		rad[i].first = theta1-theta2; rad[i].second = theta1+theta2;
-	}
-	sort(rad.begin(), rad.end(), less<pair<double, double>>());
-	for(int i=0;i<N;i++){nrad[i].first = fmod(rad[i].first + M_PI, 2*M_PI); nrad[i].second = fmod(rad[i].second + 3*M_PI, 2*M_PI);}
-
-	int ret = (int)2e9;
-	for(int i=0;i<N;i++){
-		double start = rad[i].first, end = rad[i].second;
-		if(start<=0&&end>=0){
-			int temp = lsolve(nrad, nrad[i].second, nrad[i].first);
-			cout<<temp<<endl;
-			ret = min(ret, 1 + temp);
+int check(int Y, int X, int r){
+	int r=R,c=C;
+	if(r>1){r=C;c=R;}
+	for(int y=0;y<r;y++){
+		for(int x=0;x<c;x++){
+			if(P[r][y][x]=='#'&&board[y+Y][x+X]=='#')return 0;
 		}
 	}
-	return ret;
+	return 1;
+}
+
+int solve(){
+	int ret = 0,r=R,c=C;
+	for(int r=0;r<4;r++){
+		if(r>1){r=C;c=R;}
+		for(int y=0;y<=H-R;y++){
+			for(int x=1-C;x<=W-C;x++){
+				if(check(y,x,r)){
+					
+				}
+			}
+		}
+	}
 }
 
 int main(){
 	int T;cin>>T;
 	while(T--){
-		cin>>N;vector<pair<double, pair<double, double>>> sol(N);
-		for(int i=0;i<N;i++)scanf("%lf%lf%lf",&sol[i].second.second, &sol[i].second.first, &sol[i].first);
-		int temp = csolve(sol);
-		if(temp==(int)2e9)cout<<"IMPOSSIBLE"<<endl;
-		else cout<<temp<<endl;
+		cin>>H>>W>>R>>C;
+		for(int i=0;i<H;i++)scanf("%s",board[i]);
+		for(int i=0;i<R;i++)scanf("%s",piece[i]);
+		piece_opti();
+		cout<<solve()<<endl;
 	}
 	return 0;
 }
