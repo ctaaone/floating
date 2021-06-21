a0336a0e6f65d72eb9e345cd807f3d8ff79d3748
Guest <guest-ncb6jc@h29865.sgb.skir.kr>
2021-06-21 19:34:52 +0900

    #41

--- algorithm/main.cpp
+++ algorithm/main.cpp
@@ -1,93 +1,67 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <utility>
 
 using namespace std;
 
-int N, M, best;
-vector<string> member;
-vector<vector<string>> food;
-vector<vector<int>> edible;
+typedef struct HINTSTRUCT{
+	int y; int x; int dir; int sum;
+} HINTSTRUCT;
 
-bool isOver(vector<int> canEat){
-	for(int i=0;i<canEat.size();i++)
-		if(canEat[i] == 0) return false;
-	return true;
-}
+vector<vector<int>> board;
+vector<HINTSTRUCT> HINT;
 
-vector<int> cook(int foodIndex, vector<int> canEat){
-	vector<string> list = food[foodIndex];
-
-	for(int i=0;i<list.size();i++){
-		for(int j=0;j<member.size();j++)
-			if(member[j] == list[i]){
-				canEat[j] = 1; break;
-			}
-	}
-	return canEat;
-}
-
-void calculEadible(){
-	edible.resize(member.size());
-	for(int i=0;i<food.size();i++){
-		vector<string> list = food[i];
-
-		for(int j=0;j<list.size();j++){
-			for(int l=0;l<member.size();l++){
-				if(member[l] == list[j]){
-					edible[l].push_back(i);
-					break;
-				}
-			}
-		}
-	}
-}
+int N, Q;
 
 int min(int a, int b){
-	return a>b?a:b;
+	return a<b?a:b;
 }
 
-void solve(int cur, int num, vector<int> canEat){
-	if(num >= best) return;
-	if(cur == member.size()) return;
-	if(isOver(canEat)){
-		best = num; return;
+pair<int, int> getNextCoor(){
+	int minBlank = 2e9, nextHINT;
+	for(int i=0;i < HINT.size(); i++){
+		int num = 0, Y = HINT[i].y, X = HINT[i].x;
+		while(board[Y + (HINT[i].dir == 1?1:0)][X + (HINT[i].dir == 0?1:0)]==0){
+			num++;
+			if(HINT[i].dir == 1) Y++;
+			else X++;
+		}
+		if(num < minBlank){
+			minBlank = num;
+			nextHINT = i;
+		}
 	}
-	if(canEat[cur] == 1){solve(cur + 1, num, canEat);return;}
-	
-	vector<int> eatList = edible[cur];
-	for(int i=0; i < eatList.size(); i++)
-		solve(cur + 1, num + 1, cook(eatList[i], canEat));
+
+
+	///
+}
+
+void solve(){
 
 }
 
 int main(){
-	//freopen("input.txt","r",stdin);
-	int T; cin>>T;
-	
+	freopen("input.txt", "r", stdin);
+	int T;cin>>T;
 	while(T--){
-		best = (int)2e9; member.clear(); for(int i=0;i<food.size();i++)food[i].clear(); food.clear();
-		for(int i=0;i<edible.size();i++)edible[i].clear(); edible.clear();
+		for(int i=0;i<board.size();i++)board[i].clear(); board.clear(); HINT.clear();
 		
-		cin>>N>>M;
+		cin>>N;
 		for(int i=0;i<N;i++){
-			string temp;
-			cin>>temp; member.push_back(temp);
-		}
-		for(int i=0;i<M;i++){
-			int L; cin>>L;
-			vector<string> tempv;
-			string temp;
-			for(int l=0;l<L;l++){
-				cin>>temp; tempv.push_back(temp);
+			int tempINT; vector<int> tempVector;
+			for(int j=0;j<N;j++){
+				scanf("%d",&tempINT); if(tempINT == 1) tempINT = -1; tempVector.push_back(tempINT);
 			}
-			food.push_back(tempv);
+			board.push_back(tempVector);
 		}
-
-		calculEadible();
-		vector<int> canEat(member.size(), 0);
-		solve(0, 0, canEat);
-		cout<<best<<endl;
+		cin>>Q;
+		for(int i=0;i<Q;i++){
+			HINTSTRUCT tempHINT;
+			scanf("%d%d%d%d", &tempHINT.y, &tempHINT.x, &tempHINT.dir, &tempHINT.sum);
+			HINT.push_back(tempHINT);
+		}
+		
 	}
-	return 0;
+	return 1;
 }
