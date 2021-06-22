7e210ea32a413830ca3bda69f77067e6ff5cd5b1
Guest <guest-czrpt4@h29865.sgb.skir.kr>
2021-06-22 19:32:39 +0900

    #42

--- algorithm/main.cpp
+++ algorithm/main.cpp
@@ -11,6 +11,7 @@ typedef struct HINTSTRUCT{
 
 vector<vector<int>> board;
 vector<HINTSTRUCT> HINT;
+int hintMap[20][20][2];
 
 int N, Q;
 
@@ -22,23 +23,91 @@ pair<int, int> getNextCoor(){
 	int minBlank = 2e9, nextHINT;
 	for(int i=0;i < HINT.size(); i++){
 		int num = 0, Y = HINT[i].y, X = HINT[i].x;
-		while(board[Y + (HINT[i].dir == 1?1:0)][X + (HINT[i].dir == 0?1:0)]==0){
-			num++;
+		if(HINT[i].dir == 1) Y++;
+		else X++;
+		
+		while(Y<N && X<N && board[Y][X] != -1){
+			if(board[Y][X] == 0)num++;
 			if(HINT[i].dir == 1) Y++;
 			else X++;
 		}
-		if(num < minBlank){
+
+		if(num != 0 && num < minBlank){
 			minBlank = num;
 			nextHINT = i;
 		}
 	}
-
-
-	///
+	int Y = HINT[nextHINT].y, X = HINT[nextHINT].x, dir = HINT[nextHINT].dir;
+	if(dir == 1) Y++;
+	else X++;
+	while(board[Y][X] != 0){
+		if(dir == 1) Y++;
+		else X++;
+	}
+	return make_pair(Y,X); 
 }
 
-void solve(){
+bool isOver(){
+	for(int i=0; i<N; i++)
+		for(int j=0; j<N; j++)
+			if(board[i][j] == 0)return false;
+	return true;
+}
 
+bool checkNum(int NUM, int y, int x){
+	int dy[] = {-1, 0, 1, 0}, dx[] = {0, 1, 0, -1}, overFlagH = 1, overFlagV = 1;
+	for(int dir = 0; dir<4 ; dir++){
+		int Y = y + dy[dir], X = x + dx[dir];
+		while(Y>=0 && Y<N && X>=0 && X<N && board[Y][X] != -1){
+			if(board[Y][X] == 0) {
+				if(dir%2 == 0) overFlagV = 0;
+				else overFlagH = 0;
+			}
+			if(board[Y][X] == NUM) return false;
+			Y += dy[dir]; X += dx[dir];
+		}
+	}
+
+	if(overFlagV == 1){
+		int sum = 0, curY = y - 1, curX = x;
+		while(board[curY][curX] != -1)curY--;
+		int hintSum = hintMap[curY][curX][1];
+		while(curY<N && board[curY][curX] != -1){
+			sum += (board[curY][curX] == 0?NUM:board[curY][curX]); curY ++ ;
+		}
+		if(sum != hintSum)return false;
+	}
+	if(overFlagH == 1){
+		int sum = 0, curY = y, curX = x - 1;
+		while(board[curY][curX] != -1)curX--;
+		int hintSum = hintMap[curY][curX][0];
+		while(curX<N && board[curY][curX] != -1){
+			sum += (board[curY][curX] == 0?NUM:board[curY][curX]); curX ++ ;
+		}
+		if(sum != hintSum){cout<<sum<<" "<<hintSum<<endl;return false;}
+	}
+	return true;
+}
+
+
+bool solve(){
+	if(isOver()) return true;
+	int nextY, nextX; pair<int, int> nextCoor;
+	nextCoor = getNextCoor();
+	nextY = nextCoor.first; nextX = nextCoor.second;
+	for(int NUM = 1; NUM < 10 ; NUM++){
+		if(checkNum(NUM, nextY, nextX)){
+			board[nextY][nextX] = NUM;
+			if(solve()) {cout<<"#";return true;}
+			board[nextY][nextX] = 0;
+		}
+	}
+	return false;
+}
+
+void setHintMap(){
+	for(int i = 0; i<HINT.size() ; i++)
+		hintMap[HINT[i].y][HINT[i].x][HINT[i].dir] = HINT[i].sum;
 }
 
 int main(){
@@ -51,7 +120,7 @@ int main(){
 		for(int i=0;i<N;i++){
 			int tempINT; vector<int> tempVector;
 			for(int j=0;j<N;j++){
-				scanf("%d",&tempINT); if(tempINT == 1) tempINT = -1; tempVector.push_back(tempINT);
+				scanf("%d",&tempINT); if(tempINT == 0) tempINT = -1; else tempINT = 0; tempVector.push_back(tempINT);
 			}
 			board.push_back(tempVector);
 		}
@@ -59,9 +128,19 @@ int main(){
 		for(int i=0;i<Q;i++){
 			HINTSTRUCT tempHINT;
 			scanf("%d%d%d%d", &tempHINT.y, &tempHINT.x, &tempHINT.dir, &tempHINT.sum);
+			tempHINT.y--;tempHINT.x--;
 			HINT.push_back(tempHINT);
 		}
-		
+		setHintMap();
+
+		solve();
+		for(int i=0;i<N;i++){
+			for(int j=0;j<N;j++){
+				if(board[i][j] == -1) printf("0 ");
+				else printf("%d ",board[i][j]);
+			}
+			printf("\n");
+		}
 	}
 	return 1;
 }
