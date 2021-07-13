78ea54301c1cf7fda2a04c6ad1da0fa69251269a
Guest <guest-oawaf8@h29865.sgb.skir.kr>
2021-07-13 19:56:10 +0900

    #48

--- algorithm/main.cpp
+++ algorithm/main.cpp
@@ -1,50 +1,16 @@
-#include <vector>
 #include <iostream>
+#include <vector>
+
 
 using namespace std;
 
 
-int N, K, maxL;
-vector<int> L, M, G; // Distance, Length, Interval
-
-int max(int a, int b){
-	return a>b?a:b;
-}
-
-bool isRight(int d){
-	int sum = 0;
-	for(int i=0;i<N;i++){
-		int start = L[i] - M[i], end = L[i];
-		if(start <= K && K <= end)
-			sum += (K-start) / G[i] + 1;
-	}
-	return sum >= K;
-}
-
-int solve(){
-	int s = 0, e = maxL, h = (s + e)/2;
-	while(s < e){
-		if(isRight(h)){
-			e = h; h = (s + e)/2;
-		}else{
-			s = h; h = (s + e)/2; 
-		}
-	}
-	return h;
-}
+int N, K;
 
 int main(){
 	int T;cin>>T;
 	while(T--){
-		L.clear(); M.clear(); G.clear(); maxL = 0;
-		cin>>N>>K;
-		for(int i=0;i<N;i++){
-			int tL, tM, tG;
-			scanf("%d%d%d", &tL, &tM, &tG);
-			maxL = max(maxL, tL);
-			L.push_back(tL); M.push_back(tM); G.push_back(tG);
-		}
-		cout<<solve()<<endl;
+
 	}
-	return 0;
+	return 1;
 }
