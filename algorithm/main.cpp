e89ccda482966dc52855599e6fc185460fe979e5
Guest <guest-j1e0ij@h29865.sgb.skir.kr>
2021-07-16 19:59:23 +0900

    #50

--- algorithm/main.cpp
+++ algorithm/main.cpp
@@ -1,41 +1,73 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <cmath>
 
 using namespace std;
 
+int N;
 
-int N, K;
-vector<int> r, c;
-
-bool isRight(double x){
-	vector<double> res(N);
-	for(int i=0;i<N;i++)res[i] = c[i] * x - r[i];
-	sort(res.begin(), res.end(), greater<double>());
-	double sum = 0;
-	for(int i=0;i<K;i++)
-		sum += res[i];
-	if(sum>=0) return true;
-	return false;
+void swap(double &a, double &b){
+	double temp = a;
+	a = b; b = temp;
 }
 
-double solve(){
-	double s = 0, e = 1, h = (s+e)/2;
-	while(e-s>1e-10){
-		if(isRight(h)) e = h;
-		else s = h;
-		h=(s+e)/2;
+vector<double> solve(vector<double> coe){
+	vector<double> ret;
+
+	if(coe.size() == 3){
+		double a = coe[0], b = coe[1], c = coe[2];
+		double one = -b / (2 * a);
+		double two = sqrt(b*b-4*a*c) / (2 * a);
+		ret.push_back(one - two); ret.push_back(one + two);
+		sort(ret.begin(), ret.end(), less<double>());
+		return ret;
 	}
-	return h;
+
+	vector<double> origin = coe;
+	for(int i=0;i<coe.size()-1;i++)coe[i] = coe[i] * (coe.size()-i-1);
+	coe.pop_back();
+	coe = solve(coe);
+
+	vector<double> section;
+	section.push_back(-10);
+	section.insert(section.end(), coe.begin(), coe.end());
+	section.push_back(10);
+
+	for(int i=0;i<section.size()-1;i++){
+		int iter = 60;
+		double s = section[i], e = section[i+1], h = (s+e)/2;
+		double Y = 0, Y1 = 0;
+		for(int i=0;i<origin.size();i++){
+			Y += origin[i] * pow(s, (origin.size()-i-1));
+			Y1 += origin[i] * pow(e, (origin.size()-i-1));
+		}
+		if(Y>Y1) swap(s,e);
+		
+		while(iter--){
+			Y = 0;
+			for(int i=0;i<origin.size();i++)
+				Y += origin[i] * pow(h, (origin.size()-i-1));
+			if(Y>0)e = h;
+			else s = h;
+			h = (s+e)/2;
+		}
+		ret.push_back(h);
+	}
+	sort(ret.begin(), ret.end(), less<double>());
+	return ret;
 }
+
 
 int main(){
 	//freopen("input.txt","r",stdin);
 	int T;cin>>T;
 	while(T--){
-		cin>>N>>K;r.clear();c.clear();
-		for(int i=0;i<N;i++){int tempr,tempc;scanf("%d%d",&tempr,&tempc);r.push_back(tempr);c.push_back(tempc);}
-		printf("%.10lf\n", solve());
+		vector<double> coe;
+		cin>>N;for(int i=0;i<=N;i++){double temp;scanf("%lf",&temp);coe.push_back(temp);}
+		vector<double> solution = solve(coe);
+		for(int i=0;i<solution.size();i++)printf("%.12lf ",solution[i]);
+		cout<<endl;
 	}
 	return 0;
 }
